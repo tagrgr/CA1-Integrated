67f40a9c74dc713e4423a90f7fa05297f8faac28
Tiago de Gregori <sba24297@student.cct.ie>
2025-01-26 17:20:35 +0000

    test

---
+++ AnimMgmtSys/src/main/java/com/mycompany/animmgmtsys/AnimMgmtSys.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.animmgmtsys;
+
+/**
+ *
+ * @author tiago
+ */
+public class AnimMgmtSys {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
